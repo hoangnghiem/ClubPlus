f495da87a7c8f9bc0a91589284a99444044baa7a
Hoang Nghiem <hoangnghiem1711@gmail.com>
2011-11-23 21:00:22 +0700

    new Rails app generated by Rails Apps Composer gem

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Clubplus::Application.config.secret_token = '6b4da562e6a300f73ffddf06b10b66816568799aa09866819a6481cec693b9dd2336919eb6d3693e1183269b6009f93e797fcf1fbcedaad14c704b61cda527b7'
